e2eeb70c12d6ce9a3146f43e80e55db35b20e50d
SurajAnthwal <suraj@zemosolabs.com>
2016-11-10 13:09:10 +0530

    Private and Group Chat completed

--- index.js
+++ index.js
@@ -5,6 +5,80 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fileNumber = 1;
+// var multer = require('multer');
+// var upload = multer({dest: './'});
+
+// var multiparty = require('connect-multiparty'),
+//     multipartyMiddleware = multiparty({ uploadDir: './UploadedFiles' });
+
+var multiparty = require('multiparty'),
+    fs = require('fs'),
+    uuid = require('node-uuid');
+
+app.post('/UploadedFiles', function (req, res) {
+    var size = '';
+    var file_name = '';
+    var destination_path = '';
+    console.log('Inside /UploadedFiles path');
+    // console.log(req);
+
+    var form = new multiparty.Form({uploadDir: 'public/UploadedFiles/'});
+    // form.parse(req, function (err, fields, files) {
+    //     res.writeHead(200, {'content-type': 'text/plain'});
+    //     res.write('received upload:\n\n');
+    //     res.end("Thanks");
+    // });
+
+    form.on('error', function (err) {
+        console.log('Error parsing form: ' + err.stack);
+    });
+
+    // Parts are emitted when parsing the form
+    form.on('part', function (part) {
+        // You *must* act on the part by reading it
+        // NOTE: if you want to ignore it, just call "part.resume()"
+        console.log("Inside part");
+        if (!part.filename) {
+            // filename is not defined when this is a field and not a file
+            console.log('got field named ' + part.name);
+            // ignore field's content
+            part.resume();
+        }
+
+        if (part.filename) {
+            // filename is defined when this is a file
+            // count++;
+            console.log('got file named ' + part.name);
+            // ignore file's content here
+            part.resume();
+        }
+
+        part.on('error', function (err) {
+            // decide what to do
+        });
+    });
+    form.parse(req, function (err, fields, files) {
+        var fromUsername, toUsername;
+        fromUsername = fields['FromUserName'][0];
+        toUsername = fields['ToUserName'][0];
+        console.log(files.file[0]);
+        var file = files.file[0];
+        var ext = file.path.substring(file.path.lastIndexOf("."), file.path.length);
+        var filePath = 'public/UploadedFiles/' + fileNumber++ + ext;
+        fs.rename(file.path, filePath);
+        socketMap[toUsername].emit('new message', {
+            FromUserName: fromUsername,
+            image: true,
+            imageUrl: filePath.substring(filePath.indexOf("/") + 1, filePath.length)
+        });
+
+        console.log('Upload completed!');
+        res.writeHead(200, {'content-type': 'text/plain'});
+        res.end('Received ' + files.length + ' files');
+    });
+});
+
 app.use(express.static(__dirname + "/public/"));
 
 app.get('/checkAvailability', function (req, res) {
@@ -21,6 +95,7 @@ app.get('/checkAvailability', function (req, res) {
 var numUsers = 0;
 var socketMap = {};
 var usersJoinedBefore = [];
+var groups = {};
 
 io.on('connection', function (socket) {
     console.log('a user connected');
@@ -31,11 +106,11 @@ io.on('connection', function (socket) {
         for (var i in socketMap) {
             if (socketMap[i] == socket) {
                 delete socketMap[i];
-                name=i;
+                name = i;
             }
         }
         socket.broadcast.emit('user left', {
-            username:name
+            username: name
         })
     });
 
@@ -71,10 +146,47 @@ io.on('connection', function (socket) {
     //4th event
     socket.on('new message', function (data) {
         //5th event
-        socketMap[data.ToUserName].emit('new message', {
+
+        if (data.isGroup) {
+            for (var i in groups[data.ToUserName].groupMembers) {
+                socketMap[groups[data.ToUserName].groupMembers[i].label].emit('new message', {
+                    FromUserName: data.FromUserName,
+                    message: data.message,
+                    isGroup: true,
+                    ToUserName:data.ToUserName
+                });
+            }
+            socketMap[groups[data.ToUserName].createdBy].emit('new message', {
+                FromUserName: data.FromUserName,
+                message: data.message,
+                isGroup: true,
+                ToUserName:data.ToUserName
+            });
+        } else {
+            socketMap[data.ToUserName].emit('new message', {
+                FromUserName: data.FromUserName,
+                message: data.message
+            });
+        }
+    });
+
+    socket.on('is typing', function (data) {
+        socketMap[data.ToUserName].emit('is typing', {
             FromUserName: data.FromUserName,
-            message: data.message
+            isTyping: data.isTyping
         });
+    });
+
+    socket.on('new group', function (data) {
+        console.log(data);
+        groups[data.firstName] = data;
+        for (var i in data.groupMembers) {
+            if (socketMap[data.groupMembers[i].label]) {
+                socketMap[data.groupMembers[i].label].emit('new group', data);
+            }
+        }
+        socketMap[data.createdBy].emit('new group', data);
+
     });
 });
 
--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-angular.module('MyApp', ['btford.socket-io', 'MyApp.config', 'cgNotify', 'ui.router'])
+angular.module('MyApp', ['btford.socket-io', 'MyApp.config', 'cgNotify', 'ui.router', 'ngFileUpload', 'ngDialog', 'angularjs-dropdown-multiselect'])
   .controller('SignUpCtrl', ['$scope', '$state', function ($scope, $state) {
     var vm = this;
     vm.user = {};
@@ -8,7 +8,57 @@ angular.module('MyApp', ['btford.socket-io', 'MyApp.config', 'cgNotify', 'ui.rou
     };
 
   }])
-  .controller('ChatCtrl', ['$scope', 'Socket', '$filter', '$stateParams', 'notify', '$timeout', function ($scope, Socket, $filter, $stateParams, notify, $timeout) {
+  .controller('DialogueController', ['$scope', 'Socket', '$filter', 'listOfFriends', 'user', function ($scope, Socket, $filter, listOfFriends, user) {
+    var vm = this;
+    // console.log("inside dialogue controller:");
+    // console.log(listOfFriends);
+    // console.log(user);
+
+    $scope.friend = [];
+    $scope.friends = [];
+    var obj = {}, id = 1;
+    angular.forEach(listOfFriends, function (value, key) {
+      obj.id = id++;
+      obj.label = value.firstName;
+      $scope.friends.push(obj);
+      obj = {};
+    });
+
+
+    $scope.settings = {
+      smartButtonMaxItems: 10
+    };
+
+    $scope.submitForm = function () {
+      angular.forEach($scope.friends, function (value, key) {
+        angular.forEach($scope.friend, function (value1, key) {
+          if (value1.id == value.id)
+            value1.label = value.label;
+        });
+
+
+      });
+      var group = {
+        groupName: $scope.grpName,
+        groupStatus: $scope.grpStatus,
+        groupParticipants: $scope.friend
+      };
+      if ($scope.friends != []) {
+        // Socket.emit('new Group', {
+        //   FromUserName: user.firstName,
+        //   groupData: group
+        // });
+        $scope.$emit('newGroup', {
+          FromUserName: user.firstName,
+          groupData: group
+        });
+      }
+      $scope.closeThisDialog();
+    }
+
+
+  }])
+  .controller('ChatCtrl', ['$scope', 'Socket', '$filter', '$stateParams', 'notify', '$timeout', 'Upload', 'ngDialog', function ($scope, Socket, $filter, $stateParams, notify, $timeout, Upload, ngDialog) {
     var vm = this;
 
     //The current user
@@ -20,6 +70,9 @@ angular.module('MyApp', ['btford.socket-io', 'MyApp.config', 'cgNotify', 'ui.rou
     //User's friends who are online and are shown active in the left bar
     vm.Friends = [];
 
+    //Group Chat
+    vm.Groups = [];
+
     //The actuall chat box for sending private one to one messages
     vm.chatBox = false;
 
@@ -28,6 +81,12 @@ angular.module('MyApp', ['btford.socket-io', 'MyApp.config', 'cgNotify', 'ui.rou
 
     //for storing chat history
     vm.chatLogs = {};
+
+    //for storing group chats history
+    vm.groupChatLogs = {};
+
+    //promise for 'is typing'
+    var inputChangedPromise;
 
 
     vm.messageDeliver = function () {
@@ -43,41 +102,123 @@ angular.module('MyApp', ['btford.socket-io', 'MyApp.config', 'cgNotify', 'ui.rou
           firstName: vm.user.firstName,
           messageBody: vm.userMessage
         };
+        if (vm.chatUser.hasLeft)
+          chatObject.hasLeft = true;
+
+        var messageObject = {
+          FromUserName: vm.user.firstName,
+          ToUserName: vm.chatUser.firstName,
+          message: vm.userMessage
+        };
 
         var x = vm.chatUser.firstName;
-        vm.chatLogs[x].push(chatObject);
-        vm.Messages = vm.chatLogs[x];
+        if (!vm.chatUser.isGroup) {
+          vm.chatLogs[x].push(chatObject);
+          vm.Messages = vm.chatLogs[x];
+          Socket.emit('new message', messageObject);
+        } else {
+          // vm.groupChatLogs[x].push(chatObject);
+          // vm.Messages = vm.groupChatLogs[x];
+          messageObject.isGroup = true;
+          Socket.emit('new message', messageObject);
+        }
         $scope.$watch('vm.Messages', function (newVal, oldVal) {
           $("#scrollBar").scrollTop($('#scrollBar')[0].scrollHeight);
         });
 
         //4th event
-        Socket.emit('new message', {
-          FromUserName: vm.user.firstName,
-          ToUserName: vm.chatUser.firstName,
-          message: vm.userMessage
-        });
+
 
         vm.userMessage = null;
       }
-    }
+    };
 
     vm.sendMessage = function (event) {
       if (event.which === 13) {
         vm.messageDeliver();
+      } else {
+        if (!vm.chatUser.isGroup) {
+          if (inputChangedPromise) {
+            $timeout.cancel(inputChangedPromise);
+            Socket.emit('is typing', {
+              FromUserName: vm.user.firstName,
+              ToUserName: vm.chatUser.firstName,
+              isTyping: true
+            });
+          }
+          inputChangedPromise = $timeout(function () {
+            Socket.emit('is typing', {
+              FromUserName: vm.user.firstName,
+              ToUserName: vm.chatUser.firstName,
+              isTyping: false
+            });
+          }, 2000);
+        }
       }
     };
 
     vm.ChatMessageCreation = function (friend) {
-      vm.chatUser = friend;
-      vm.chatUser.color = 'white';
+      vm.chatUser = angular.copy(friend);
+      friend.color = 'white';
       var x = friend.firstName;
-      vm.Messages = vm.chatLogs[x];
+      if (!vm.chatUser.isGroup)
+        vm.Messages = vm.chatLogs[x];
+      else
+        vm.Messages = vm.groupChatLogs[x];
       $scope.$watch('vm.Messages', function (newVal, oldVal) {
         $("#scrollBar").scrollTop($('#scrollBar')[0].scrollHeight);
       });
 
     };
+
+    vm.createGroupChatDialogue = function () {
+      console.log("clicked group glyphicon");
+      $scope.value = true;
+      ngDialog.open({
+        template: 'GroupChat/groupChats.html',
+        className: 'ngdialog-theme-default',
+        resolve: {
+          listOfFriends: function FRIEND() {
+            return vm.Friends;
+          },
+          user: function USER() {
+            return vm.user;
+          }
+        },
+        controller: 'DialogueController',
+        scope: $scope
+        // controllerAs: 'dc',
+
+      });
+    };
+
+    $scope.$on('newGroup', function (event, data) {
+      console.log("Inside Chat Ctrl");
+      var group = {
+        firstName: data.groupData.groupName,
+        status: data.groupData.groupStatus,
+        groupMembers: data.groupData.groupParticipants,
+        createdBy: vm.user.firstName
+      };
+      // vm.Groups.push(group);
+      // console.log(data);
+      Socket.emit('new group', group);
+    });
+    Socket.on('new group', function (data) {
+      vm.groupChatLogs[data.firstName] = [];
+      var groupObject = {};
+      groupObject = data;
+      var tooltip = "Participants: ";
+      angular.forEach(data.groupMembers, function (value, key) {
+        tooltip += value.label + ", ";
+      });
+      tooltip += data.createdBy + "\n";
+      tooltip += "Created by: " + data.createdBy;
+      groupObject.tooltip = tooltip;
+      groupObject.color = "white";
+      groupObject.isGroup = true;
+      vm.Groups.push(groupObject);
+    });
 
     //1st event
     Socket.emit('add user', {user: vm.user});
@@ -91,7 +232,8 @@ angular.module('MyApp', ['btford.socket-io', 'MyApp.config', 'cgNotify', 'ui.rou
         firstName: data.username,
         status: data.status,
         color: 'white',
-        message: ''
+        message: '',
+        isTyping: false
       });
     });
 
@@ -106,7 +248,8 @@ angular.module('MyApp', ['btford.socket-io', 'MyApp.config', 'cgNotify', 'ui.rou
         firstName: data.username,
         status: data.status,
         color: 'white',
-        message: ''
+        message: '',
+        isTyping: false
       });
       notifyMesage = x + " has come online!!!";
       notify({message: notifyMesage, duration: '3000', position: 'center'});
@@ -114,29 +257,55 @@ angular.module('MyApp', ['btford.socket-io', 'MyApp.config', 'cgNotify', 'ui.rou
 
     //5th event
     Socket.on('new message', function (data) {
-
+      console.log(data);
       var x = data.FromUserName;
       var chatObject = {
         time: $filter('date')(new Date(), 'hh:mm a, EEE'),
         firstName: data.FromUserName,
-        messageBody: data.message
       };
-      vm.chatLogs[x].push(chatObject);
+      if (data.message) {
+        chatObject.messageBody = data.message;
+      } else {
+        chatObject.image = true;
+        chatObject.imageUrl = "http://localhost:3000/" + data.imageUrl;
+      }
+      console.log("url " + chatObject.imageUrl);
 
-      if (vm.chatUser.firstName == data.FromUserName) {
-        vm.Messages = vm.chatLogs[x];
+      if (data.isGroup) {
+        chatObject.isGroup = true;
+        vm.groupChatLogs[data.ToUserName].push(chatObject);
+        // console.log("group Chat");
+        // console.log(vm.groupChatLogs);
+
+        if (vm.chatUser.firstName == data.ToUserName)
+          vm.Messages = vm.groupChatLogs[data.ToUserName];
         $scope.$watch('vm.Messages', function (newVal, oldVal) {
           $("#scrollBar").scrollTop($('#scrollBar')[0].scrollHeight);
         });
       } else {
-        angular.forEach(vm.Friends, function (value, key) {
+        vm.chatLogs[x].push(chatObject);
+        if (vm.chatUser.firstName == data.FromUserName) {
+          vm.Messages = vm.chatLogs[x];
+          $scope.$watch('vm.Messages', function (newVal, oldVal) {
+            $("#scrollBar").scrollTop($('#scrollBar')[0].scrollHeight);
+          });
+        } else {
+          angular.forEach(vm.Friends, function (value, key) {
 
-          if (vm.Friends[key].firstName == data.FromUserName) {
-            vm.Friends[key].color = 'yellow';
-          }
-        });
+            if (vm.Friends[key].firstName == data.FromUserName) {
+              vm.Friends[key].color = 'yellow';
+            }
+          });
+        }
       }
+    });
 
+    Socket.on('is typing', function (data) {
+      angular.forEach(vm.Friends, function (value, key) {
+        if (vm.Friends[key].firstName == data.FromUserName) {
+          vm.Friends[key].isTyping = data.isTyping;
+        }
+      });
     });
 
     Socket.on('user left', function (data) {
@@ -147,14 +316,60 @@ angular.module('MyApp', ['btford.socket-io', 'MyApp.config', 'cgNotify', 'ui.rou
 
         if (vm.Friends[key].firstName == x) {
           vm.Friends.splice(key, 1);
+          if (vm.chatUser.firstName == x)
+            vm.chatUser.hasLeft = true;
         }
       });
       notifyMesage = x + " has left the web chat!!!";
       notify({message: notifyMesage, duration: '3000', position: 'center'});
     });
 
-  }])
+    //image handling
+    vm.uploadFile = function (file, invalidFile) {
+      // vm.user.file = file;
+      // vm.user.errorFile = invalidFile && invalidFile[0];
+      // console.log("Uploaded");
+      console.log(file);
+      if (file) {
+        file.upload = Upload.upload({
+          url: '/UploadedFiles',
+          data: {
+            file: file,
+            FromUserName: vm.user.firstName,
+            ToUserName: vm.chatUser.firstName
+          }
+        });
+        var chatObject = {
+          time: $filter('date')(new Date(), 'hh:mm a, EEE'),
+          firstName: vm.user.firstName,
+          image: true,
+          imageFile: file
+        };
+        // console.log(file);
+        vm.chatLogs[vm.chatUser.firstName].push(chatObject);
+        vm.Messages = vm.chatLogs[vm.chatUser.firstName];
+        $scope.$watch('vm.Messages', function (newVal, oldVal) {
+          $("#scrollBar").scrollTop($('#scrollBar')[0].scrollHeight);
+        });
 
+        file.upload.then(function (response) {
+          console.log("file uploaded");
+          console.log(response);
+
+          file.result = response.data;
+        }, function (response) {
+          console.log("file upload failed");
+
+          if (response.status > 0)
+            chatObject.errorMsg = response.status + ': ' + response.data;
+        }, function (evt) {
+          chatObject.progress = Math.min(100, parseInt(100.0 *
+            evt.loaded / evt.total));
+        });
+      }
+    };
+
+  }])
   .factory('Socket', ['socketFactory', function (socketFactory) {
     return socketFactory();
   }])
@@ -182,4 +397,3 @@ angular.module('MyApp', ['btford.socket-io', 'MyApp.config', 'cgNotify', 'ui.rou
     };
   });
 
-;
